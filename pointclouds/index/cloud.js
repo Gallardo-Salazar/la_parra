54f9e898c7b53fabd1b84f8a8cd19b473a795ebe
unknown <https://github.com/Gallardo-Salazar/socavon_tlanepantla.git>
2023-11-28 13:07:06 -0700

    Initial Commit

---
+++ pointclouds/index/cloud.js
@@ -0,0 +1,43 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 104102196,
+    "boundingBox": {
+        "lx": 587492.625,
+        "ly": 2651102.25,
+        "lz": 511.54583740234377,
+        "ux": 590551.1875,
+        "uy": 2654160.8125,
+        "uz": 3570.1083374023438
+    },
+    "tightBoundingBox": {
+        "lx": 587492.59545,
+        "ly": 2651102.1521,
+        "lz": 511.5458510742187,
+        "ux": 590551.18905,
+        "uy": 2653747.6156,
+        "uz": 795.1488510742188
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        }
+    ],
+    "spacing": 26.48792839050293,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
